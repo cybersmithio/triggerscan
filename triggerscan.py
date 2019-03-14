896d204626232ab17b4ef747c65830e6a75a4b72
James <james@quizlicious.com>
2019-03-14 13:43:37 -0400

    Updated for Python3, and uses pyTenable library now.  Can launch scans in Tenable.io or Tenable.sc, using the scan name or scan ID.  (Have not had a chance to test with Tenable.sc)  Also added argparse.

--- triggerscan.py
+++ triggerscan.py
@@ -5,10 +5,14 @@
 # or read from environment variables.  It will not read passwords
 # from the command line.
 #
-# Version 1.0
+# Version 1.1 - 2019-03-14
+#        Updated to use pytenable library, use argparse, and support launching scans in Tenable.io
 #
-# Roadmap
-#   
+# Version 1.0 - Initial version, written by James Smith
+#
+# Requires the following:
+#   pip install pytenable
+#
 # Sample usage:
 #
 # SCHOST=192.168.1.1; export SCHOST
@@ -21,19 +25,17 @@
 
 import sys
 import os
-import re
-import string
 import json
-from datetime import datetime,date, time
-import requests
-from securitycenter import SecurityCenter5
-		
-		
+import argparse
+from tenable.io import TenableIO
+from tenable.sc import TenableSC
+
+
 ################################################################
 # Description: Launches a scan by name
 ################################################################
 # Input:
-#        scsm = the SecurityCenter Security Manager session object
+#        conn = the connection handle to Tenable.sc or Tenable.io
 #        scan = The name of the scan to launch
 #
 ################################################################
@@ -45,119 +47,223 @@ from securitycenter import SecurityCenter5
 #        Put the scan name in the scan list filter
 #
 ################################################################
-def LaunchScan(scsm,scan):
-	#First upload the XML file, then tell SC to import it.
-	DEBUG=False
+def LaunchScan(DEBUG,conn,scanname,scanid):
 	if DEBUG:
-		print "Launching scan",scan
+		if scanid == "":
+			print("Launching scan", scanname)
+		else:
+			print("Launching scan",scanid)
 
-	resp=scsm.get('scan?filter=usable&fields=canUse%2CcanManage%2Cowner%2Cgroups%2CownerGroup%2Cstatus%2Cname%2CcreatedTime%2Cschedule%2Cpolicy%2Cplugin%2Ctype')
+	#Determine if we connected to Tenable.io or Tenable.sc
+	TIO = False
+	if str(type(conn)) == "<class 'tenable.io.TenableIO'>":
+		TIO = True
 
-	if DEBUG:
-		print resp
-		print resp.text
-	respdata=json.loads(resp.text)
-	scanlist=respdata['response']['usable']
-	if DEBUG:
-		print "\n\nResponse error code/error message",respdata['error_code'],"/",respdata['error_msg']
-		print "\n\nScan list",scanlist
-
-	scanid=0
-	for i in scanlist:
-		if DEBUG:
-			print "Scan:"
-			print "id:",i['id']
-			print "name:",i['name']
-			print "\n"
-		if i['name'] == scan:
-			scanid=int(i['id'])
-			if DEBUG:
-				print "Found scan! ID is:",i['id']
-			
-
-	if scanid != 0:
-		if DEBUG:
-			print "Scan ID is:",str(scanid)
-		resp=scsm.post('scan/'+str(scanid)+'/launch')
+	if TIO:
+		if scanid == "":
+			for scan in conn.scans.list():
+				if scan['name'] == scanname:
+					print("Found scan ID",scan['id'],"for scan name",scan['name'])
+					scanid=int(scan['id'])
+		conn.scans.launch(scanid)
 		return(True)
 	else:
-		print "Scan not found"
+		if scanid == "":
+			for scan in conn.scans.list():
+				if scan['name'] == scanname:
+					print("Found scan ID",scan['id'],"for scan name",scan['name'])
+					scanid=int(scan['id'])
+		conn.scans.launch(scanid)
+		return(True)
 
 	return(False)
 
-		
+#Attempts to make a connection to Tenable.sc
+def ConnectSC(DEBUG,username,password,host,port):
+	#Create the connection to Tenable.sc
+	try:
+		sc = TenableSC(host, port=port)
+	except:
+		print("Error connecting to SecurityCenter", sys.exc_info()[0], sys.exc_info()[1])
+		return(False)
+
+	try:
+		sc.login(username, password)
+	except:
+		print("Error logging into to SecurityCenter", sys.exc_info()[0], sys.exc_info()[1])
+		if DEBUG:
+			print("Username:",username)
+		return (False)
+
+	return(sc)
+
+
+#Attempts to make a connection to Tenable.io
+def ConnectIO(DEBUG,accesskey,secretkey,host,port):
+	#Create the connection to Tenable.io
+	try:
+		tio=TenableIO(accesskey, secretkey)
+	except:
+		print("Error connecting to Tenable.io")
+		return(False)
+
+	return(tio)
+
 
 ################################################################
 # Start of program 
 ################################################################
 #Set debugging on or off
 DEBUG=False
+parser = argparse.ArgumentParser(description="Launches a scan in Tenable.sc or Tenable.io.")
+parser.add_argument('--accesskey',help="The Tenable.io access key",nargs=1,action="store")
+parser.add_argument('--secretkey',help="The Tenable.io secret key",nargs=1,action="store")
+parser.add_argument('--username',help="The SecurityCenter username",nargs=1,action="store")
+parser.add_argument('--password',help="The SecurityCenter password",nargs=1,action="store")
+parser.add_argument('--host',help="The Tenable host. (Default for Tenable.io is cloud.tenable.com)",nargs=1,action="store")
+parser.add_argument('--port',help="The Tenable port. (Default is 443)",nargs=1,action="store")
+parser.add_argument('--scanname',help="The name of the scan to launch.",nargs=1,action="store")
+parser.add_argument('--scanid',help="The scan ID of the scan to launch.",nargs=1,action="store")
+parser.add_argument('--debug',help="Turn on debugging",action="store_true")
+args=parser.parse_args()
 
-#Pull as much information from the environment variables
-# as possible, and where missing then initialize the variables.
-if os.getenv('SCHOST') is None:
-	schost=""
+
+if args.debug:
+	DEBUG=True
+	print("Debugging is enabled.")
+
+
+# Pull as much information from the environment variables about the system to which to connect
+# Where missing then initialize the variables with a blank or pull from command line.
+if os.getenv('TIO_ACCESS_KEY') is None:
+	accesskey = ""
 else:
-	schost=os.getenv('SCHOST')
+	accesskey = os.getenv('TIO_ACCESS_KEY')
+
+# If there is an access key specified on the command line, this override anything else.
+try:
+	if args.accesskey[0] != "":
+		accesskey = args.accesskey[0]
+except:
+	nop = 0
+
+if os.getenv('TIO_SECRET_KEY') is None:
+	secretkey = ""
+else:
+	secretkey = os.getenv('TIO_SECRET_KEY')
+# If there is an  secret key specified on the command line, this override anything else.
+try:
+	if args.secretkey[0] != "":
+		secretkey = args.secretkey[0]
+except:
+	nop = 0
+
+
+username=""
+#Look for a Tenable.io username
+if os.getenv('SC_USERNAME') is None:
+	username = ""
+else:
+	username = os.getenv('SC_USERNAME')
 	if DEBUG:
-		print "Found SCHOST variable:",schost
-
-if os.getenv('SCUSERNAME') is None:
+		print("Detected SC username")
+try:
+	if args.username[0] != "":
+		username = args.username[0]
+		if DEBUG:
+			print("Detected SC username")
+		#Since a specific username was found on the command line, assume the user does not want to poll Tenable.io
+		secretkey = ""
+		accesskey = ""
+except:
 	username=""
+
+#Look for a SecurityCenter password
+scpassword=""
+if os.getenv('SC_PASSWORD') is None:
+	scpassword = ""
 else:
-	username=os.getenv('SCUSERNAME')
+	scpassword = os.getenv('SC_PASSWORD')
+	if DEBUG:
+		print("Detected SC password")
+try:
+	if args.password[0] != "":
+		if DEBUG:
+			print("Detected SC password")
+		scpassword = args.password[0]
+except:
+	scpassword=""
 
-if os.getenv('SCPASSWORD') is None:
-	password=""
+#Look for a port
+port="443"
+try:
+	if args.port[0] != "":
+		port = args.port[0]
+except:
+	port = "443"
+
+#Look for a host
+host="cloud.tenable.com"
+try:
+	if args.host[0] != "":
+		host = args.host[0]
+except:
+	host = "cloud.tenable.com"
+
+scanname=""
+try:
+	if args.scanname[0] != "":
+		scanname = args.scanname[0]
+except:
+	scanname = ""
+
+scanid=""
+try:
+	if args.scanid[0] != "":
+		scanid = int(args.scanid[0])
+except:
+	scanid = ""
+
+
+if scanname == "" and scanid == "":
+	print("Need a scan name or scan ID to launch a scan:",scanid, scanname)
+	exit(-1)
+
+if scanid == "":
+	print("Connecting to",host,"to launch scan ",scanname)
 else:
-	password=os.getenv('SCPASSWORD')
-
-if os.getenv('SCSCAN') is None:
-	scan=""
-else:
-	scan=os.getenv('SCSCAN')
-
-if DEBUG:
-	print "Connecting to",schost,"as",username,"to launch scan",scan
-
-#Pull information from command line.  If nothing there,
-# and there was nothing in the environment variables, then ask user.
-if len(sys.argv) > 1:
-	schost=sys.argv[1]
-else:
-	if schost == "":
-		schost=raw_input("SC Host:")
-
-if len(sys.argv) > 2:
-	username=sys.argv[2]
-else:
-	if username == "":
-		username=raw_input("Username:")
-
-if len(sys.argv) > 3:
-	scan=sys.argv[3]
-else:
-	if scan == "":
-		scan=raw_input("Scan To Launch:")
-
-if password == "":
-	password=raw_input("Password:")
+	print("Connecting to",host,"to launch scan ID",scanid)
 
 
-print "Connecting to",schost,"as",username,"to launch scan",scan
+if accesskey != "" and secretkey != "":
+	print("Connecting to cloud.tenable.com with access key", accesskey, "to report on assets")
+	try:
+		if args.host[0] != "":
+			host = args.host[0]
+	except:
+		host = "cloud.tenable.com"
+	conn = ConnectIO(DEBUG, accesskey, secretkey, host, port)
+elif username != "" and scpassword != "":
+	if DEBUG:
+		print("Attempting to open connection to SC")
+	try:
+		if args.host[0] != "":
+			host = args.host[0]
+	except:
+		host = "127.0.0.1"
+	print("Connecting to SecurityCenter with username " + str(username) + " @ https://" + str(host) + ":" + str(port))
+	conn = ConnectSC(DEBUG, username, scpassword, host, port)
 
-#Create a session as the user
-scsm=SecurityCenter5(schost)
-scsm.login(username,password)
-if DEBUG:
-	print "Logged in as "+str(username)+" to SecurityCenter at "+str(schost)
+if conn == False:
+	print("There was a problem connecting.")
+	exit(-1)
 
 #Upload demo dashboards
-if LaunchScan(scsm,scan):
-	print "Scan launched"
+if LaunchScan(DEBUG,conn,scanname,scanid):
+	print("Scan launched")
 	exit(0)
 else:
-	print "Scan not launched"
+	print("Scan not launched")
 	exit(-1)
 
 
